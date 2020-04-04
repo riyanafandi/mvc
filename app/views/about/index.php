59da096b7f1523ee4a820d0ccb16f04995d5ca7f
Riyan Afandi <riyanafandi.sc@gmail.com>
2020-04-04 21:50:08 +0700

    mengakses gambar pada folder public

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -2,5 +2,6 @@
     <div class="jumbotron">
         <h1>Halaman About Me</h1>
         <p>Hai nama saya <?= $data["nama"]; ?>, Saya Adalah <?= $data["status"]; ?></p>
+        <img src="<?= BASEURL; ?>img/3.png" alt="riyan afandi">
     </div>
 </div>
